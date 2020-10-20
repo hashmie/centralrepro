45aa13dfb9279ea1a06a662cc1e56652e603e4ee
root <root@ip-172-31-2-175.ap-south-1.compute.internal>
2020-10-20 08:40:30 +0000

    chef commit

--- apache-cookbook/recipes/apache-recipe.rb
+++ apache-cookbook/recipes/apache-recipe.rb
@@ -9,7 +9,7 @@ package 'httpd' do
 end
 
 file '/var/www/html/index.html' do
-  content "<h1>now iam getting this chef class<h2/>"
+  content "<h1>now iam integrating chef with git<h2/>"
   action :create
 end
 
